db92327b5af0cc79a772f80b5b5afd69e2f863b7
DeloresLin <linzhibo6@gmail.com>
2024-09-28 23:48:13 +0800

    add random-number-project

--- random-number-project/randomNumber.js
+++ random-number-project/randomNumber.js
@@ -4,13 +4,15 @@ document.querySelector(".confirm").addEventListener("click", function () {
     inputNumber = Number(document.querySelector(".input").value);
     randomNumber = Math.floor(Math.random() * inputNumber + 1);
     if (randomNumber < 10) {
-        document.querySelector(".randomNumber").textContent = `   ${randomNumber}  `;
-    } else { document.querySelector(".randomNumber").textContent = randomNumber; }
+        document.querySelector(".randomNumber").textContent = `0${randomNumber}`;
+    } else {
+        document.querySelector(".randomNumber").textContent = randomNumber;
+    }
 }
     )
 
 //reset
 document.querySelector(".reset").addEventListener("click", function () {
-    document.querySelector(".randomNumber").textContent = " ? ";
+    document.querySelector(".randomNumber").textContent = "  ?  ";
     document.querySelector(".input").textContent = "";
     });
